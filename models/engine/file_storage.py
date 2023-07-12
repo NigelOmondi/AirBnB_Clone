3c639468e4bcf74ea2b7e09541520a9c60fbf370
Nigel Omondi <nigelomondi62@gmail.com>
2023-07-12 12:53:07 +0300

    Update FileStorage to manage correctly serialization and deserialization new classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,6 +2,12 @@
 """This module is for the FileStorage class."""
 import json
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 class FileStorage:
