f152e337ef5fe0047aff495c9ee3531cd973e343
Imukua <ianmukuaa@gmail.com>
2023-07-15 15:02:48 +0300

    more fixes

--- models/base_model.py
+++ models/base_model.py
@@ -26,8 +26,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            ISOformat = "%Y-%m-%dT%H:%M:%S.%f"
+            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
