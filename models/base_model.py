75de6429112d2dc89f18bd6de949cbefb5f5fb9c
Imukua <ianmukuaa@gmail.com>
2023-07-15 15:14:16 +0300

    test fix

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defines a class BaseModel."""
-from datetime import date, datetime
+from datetime import datetime
 from uuid import uuid4
 import models
 
@@ -14,7 +14,6 @@ class BaseModel:
                 :: *args(*): Unutilised.
                 :: **kwargs(dict): A Key/Value pair of arguments/attributes
         """
-
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -32,18 +31,15 @@ class BaseModel:
     def __str__(self):
         """Returns a string representation of an instance."""
         classname = self.__class__.__name__
-
         return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
 
     def save(self):
         """Updates the updated_at attribute."""
-
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """Returns the dictionary representation of an instance."""
-
         tempdict = self.__dict__.copy()
         tempdict["created_at"] = self.created_at.isoformat()
         tempdict["updated_at"] = self.updated_at.isoformat()
